a84fde5cd79b4e591a0150fab11499870aac3f3c
dersario <vanya20061482@gmail.com>
2024-10-13 16:37:35 +0400

    u

--- solution/task_4.py
+++ solution/task_4.py
@@ -2,7 +2,6 @@ def pipeline(stages: list[int], details: list[int]) -> list[int]:
     full_time = sum(stages)
     max_time = max(stages)
     sorted_details = sorted(details)
+    first_detail = min(details)
     for i in range(len(details)):
-        details[i] = sorted_details.index(details[i])*max_time+full_time
-    print(details)
-pipeline([1],[0])
+        details[i] = sorted_details.index(details[i])*max_time+full_time+first_detail
